6c87c1ce0ca1900ff0f52aa58a8b386211f4ad01
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-23 16:26:09 +0800

    function collection

--- feature-fp.clj
+++ feature-fp.clj
@@ -5,3 +5,11 @@
 
 ;; anonymous function
 (println (map (fn [x] (+ x 42)) [1 2 3]))
+
+(defn register [] 
+  "register")
+(defn checkout [] 
+  "checkout")
+(defn pay [] 
+  "pay")
+(println ((nth [register checkout pay] 1)))
