7ae82b3b8f118232f97b5aeae2c7bfe61fe9531f
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-26 08:55:44 +0800

    conj list and conj vector difference

---
+++ vector/conj-list-vs-conj-vector.clj
@@ -0,0 +1,11 @@
+(def poems '("Iliad" "Odyssey" "Now We Are Six"))
+
+(println (conj poems "Jabberwocky"))
+
+(println poems)
+
+(def vector-poems ["Iliad" "Odyssey" "Now We Are Six"])
+
+(println (conj vector-poems "Jabberwocky"))
+
+(println vector-poems)
