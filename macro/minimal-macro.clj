b80d4f03816d74353cae6f9641f14d70a809eb4f
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-27 08:44:04 +0800

    minimal macro

--- macro/minimal-macro.clj
+++ macro/minimal-macro.clj
@@ -1,4 +1,13 @@
 (defmacro minimal-macro [] 
   '(println "I'm trapped inside a macro!"))
 
+(defn minimal-function []
+  (println "I'm trapped inside a function!"))
+
 (minimal-macro)
+
+(minimal-function)
+
+(println (macroexpand '(minimal-function)))
+
+(println (macroexpand '(minimal-macro)))
