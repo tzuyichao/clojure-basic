560c2721451136598c084d0d8ffee417c77dd34f
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-27 14:46:02 +0800

    staying out of trouble

--- map/trouble.clj
+++ map/trouble.clj
@@ -1,7 +1,7 @@
-(def book {:title "Hard Times"
-           :author "Dickens"
+(def book {:author "Dickens"
+           :title "Hard Times"
            :published 1838})
 
-(println (first book))
-(println (rest book))
+(println (first book))      ; 我都是第一個，但書上說可能
+(println (rest book))       ; 我都是第一個之後的，但書上說可能是別的
 (println (count book))
