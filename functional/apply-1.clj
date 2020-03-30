52509a83dde2a41c84e701cb375eb16ac387a9f5
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-30 12:06:15 +0800

    partial labs

--- functional/apply-1.clj
+++ functional/apply-1.clj
@@ -7,6 +7,11 @@
 (def my-inc-2 (partial + 1))
 
 (println (my-inc-1 41))
-(println (my-inc-2 41))
+(println (my-inc-2))            ;; [] (f arg1)
+(println (my-inc-2 41))         ;; [x] (f arg1 x)
+(println (my-inc-2 41 42 43))   ;; [x y z] (f arg1 x y z)
 
 (println (/ (* 80000 30) 10000.0))
+
+;; partial return function call. in this example apply (partial f arg1)
+(println (partial + 1))
