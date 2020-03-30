0893abac1db253e8da7ca9acb2936d773d7c41c0
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-30 13:27:25 +0800

    update-in

---
+++ functional/update-1.clj
@@ -0,0 +1,26 @@
+(def book {:title "Emma" :copies 1000})
+
+(def new-book (update book :copies inc))
+
+(println "book:" book)
+(println "new-book:" new-book)
+
+(def by-author
+  {:name "Jane Austen"
+   :book {:title "Emma" :copies 1000}})
+
+(def by-author2
+  {:name "Jane Austen"
+   :emma {:title "Emma" :copies 1000} 
+   :s-and-s {:title "Sense and Sensibility" :copies 10000}})
+
+(def new-by-author (update-in by-author [:book :copies] inc))
+
+(println by-author)
+(println new-by-author)
+
+;;(def new-by-author2 (map (fn [x] (update x :copies inc)) (:books by-author2)))
+(def new-by-author2 (update-in by-author2 [:emma :copies] inc))
+
+(println by-author2)
+(println new-by-author2)
