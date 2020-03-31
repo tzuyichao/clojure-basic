2bd0bf95a3aa2d2a49643b9621ff52020fbcc1eb
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-31 09:20:08 +0800

    dynamic binding

--- def/examples.clj
+++ def/examples.clj
@@ -1,1 +1,18 @@
 (def PI 3.14)
+(def ^:dynamic debug-enabled false)
+
+(defn book-description [book]
+  (str (:title book) " written by " (:author book)))
+
+(def book-desc
+  (fn [book]
+    (str (:title book) " written by " (:author book))))
+
+(defn debug [msg]
+  (if debug-enabled
+    (println msg)))
+
+(binding [debug-enabled true]
+  (debug "Calling..."))
+
+(debug "you cannot see me") 
