42ff4072010dbfd96233f993b4d7d171ca2d55cc
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-31 14:48:15 +0800

    sequences lab

---
+++ sequences/examples.clj
@@ -0,0 +1,12 @@
+(def title-seq (seq ["Emma" "Oliver Twist" "Robinson Crusoe" "2012"]))
+
+(println title-seq)
+
+(def sorted-title (sort title-seq))
+
+(println sorted-title)
+(println title-seq)
+
+(println (seq []))
+(println (seq '()))
+(println (seq {}))
