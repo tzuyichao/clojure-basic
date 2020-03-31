a0241a81765a58ee44b70008a0f9e3c386de7f6e
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-31 10:57:49 +0800

    require

--- namespace/examples.clj
+++ namespace/examples.clj
@@ -12,3 +12,11 @@
 (def emma {:title "Emma" :price 9.99})
 (println "Using pricing/discount-price:" (pricing/discount-price emma))
 (println "In user namespace:" (* (- 1.0 discount-rate) (:price emma)))
+
+(def literature ["Emma" "Oliver Twist" "Possession"])
+
+(def horror ["It" "Carry" "Possession"])
+
+(require 'clojure.data)
+
+(println (clojure.data/diff literature horror))
