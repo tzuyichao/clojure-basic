f12acb4ea75a1cfff13eb27fd3840b3234274f48
TzuYi Chao <tzuyichao@mail2000.com.tw>
2020-04-02 00:36:14 +0800

    nested arrow

--- sequences/examples.clj
+++ sequences/examples.clj
@@ -50,3 +50,15 @@
 (println (apply str (interpose " // " (map :title (take 3 (reverse (sort-by :rating books)))))))
 
 (apply (fn [& x] (println x)) (interpose " // " (map :title (take 3 (reverse (sort-by :rating books))))))
+
+(defn format-top-titles [books]
+  (->>
+    books
+    (sort-by :rating)
+    reverse
+    (take 3)
+    (map :title)
+    (interpose " // ")
+    (apply str)))
+
+(println (format-top-titles books))
