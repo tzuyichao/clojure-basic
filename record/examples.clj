d67eed0c38189b0055a60d99b1c5942d016d3aa2
TzuYi Chao <tzuyichao@mail2000.com.tw>
2020-04-04 15:31:38 +0800

    record

--- record/examples.clj
+++ record/examples.clj
@@ -3,3 +3,15 @@
 (def watson (->FictionalCharacter "John Watson" "Sign of the Four" "Doyle"))
 
 (println watson)
+
+(println (:name watson))
+
+(println (count watson))
+
+(println (keys watson))
+
+(def another-watson (assoc watson :appears-in "Sherlock Holmes"))
+
+(println "watson:" watson)
+
+(println "another-watson:" another-watson)
