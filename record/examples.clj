9aa0ee841e4ccd291184744ee8000aae9ad9f22b
TzuYi Chao <tzuyichao@mail2000.com.tw>
2020-04-04 15:53:35 +0800

    more record

--- record/examples.clj
+++ record/examples.clj
@@ -15,3 +15,13 @@
 (println "watson:" watson)
 
 (println "another-watson:" another-watson)
+
+(defrecord SuperComputer [cpu no-cpus storage-db])
+
+(def watson-2 (map->SuperComputer {:cpu "Power7" :no-cpus 2880 :storage-db 4000}))
+
+(println (class watson))
+(println (class watson-2))
+
+(println (instance? FictionalCharacter watson))
+(println (instance? SuperComputer watson))
