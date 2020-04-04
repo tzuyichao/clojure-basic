4b51ae6f8e2ff11fc7edb0dc25aae69615598fac
TzuYi Chao <tzuyichao@mail2000.com.tw>
2020-04-05 00:07:05 +0800

    lab

--- sequences/examples.clj
+++ sequences/examples.clj
@@ -62,3 +62,12 @@
     (apply str)))
 
 (println (format-top-titles books))
+
+(defn find-by-title
+  "Search for a book by title,
+  where title is a string and books is a collection
+  of book maps, each of which must have :title entry"
+  [title books]
+  (some #(when (= (:title %) title) %) books))
+
+(println (find-by-title "Emma" books))
