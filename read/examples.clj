047c191df91881e1433a111a081bc9182ede9547
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-07 14:45:24 +0800

    with-meta

--- read/examples.clj
+++ read/examples.clj
@@ -14,3 +14,13 @@
 (eval (read-string s))
 
 (print-greeting true)
+
+(def books1 (with-meta ["Emma" "1984"] {:favorite-books true}))
+
+(println books1)
+(println (meta books1))
+
+(def books2 ^:favorite-books ["Emma" "1984"])
+
+(println books2)
+(println (meta books2))
