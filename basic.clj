a4554c83b9b9edaa7c82d6b16d90e756cf4758c9
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-08 12:02:48 +0800

    lab

--- basic.clj
+++ basic.clj
@@ -5,3 +5,12 @@
 
 (println (testing "   "))
 (println (testing "   12 "))
+
+;; cons
+(def r1 (cons 3 (cons 4 (cons 5 nil))))
+
+(println r1)
+
+(def uuid1 (java.util.UUID/randomUUID))
+
+(println uuid1)
