ab64cb73edad49e9b5a76c6850d3a3b07e5272de
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-08 14:39:51 +0800

    list and vector

--- list/examples.clj
+++ list/examples.clj
@@ -1,11 +1,19 @@
-(def list1 [:jar1 1 2 3 :jar2])
+;; Vector Example
+(def vec1 [:jar1 1 2 3 :jar2])
 
-(println (first list1))
-(println (rest list1))
-;; Syntax error (IndexOutOfBoundsException)
-;(println (nth list1 -13))
-(println (nth list1 3))
-;; Syntax error (IndexOutOfBoundsException)
-;(println (nth list1 13))
+(println (type vec1))
 
-(println (last list1))
+(println (first vec1))
+(println (rest vec1))
+;; Syntax error (IndexOutOfBoundsException)
+;(println (nth vec1 -13))
+(println (nth vec1 3))
+;; Syntax error (IndexOutOfBoundsException)
+;(println (nth vec1 13))
+
+(println (last vec1))
+
+;; List Example
+(def list1 '(:jar1 1 2 3 :jar2))
+
+(println (type list1))
