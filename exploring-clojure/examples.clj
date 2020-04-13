3728e98e56c4f16c830f1136c689526f268a10a0
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-13 16:16:01 +0800

    boolean

--- exploring-clojure/examples.clj
+++ exploring-clojure/examples.clj
@@ -13,3 +13,8 @@
 (println "another\nmultiline\nstring")
 
 (println (str \h \e \y \space \y \o \u))
+
+(println (if 0 "zero is true" "zero is false"))
+(println (if nil "nil is true" "nil is false"))
+(println "(true? 0):" (true? 0))
+(println "(false? 0):" (false? 0))
