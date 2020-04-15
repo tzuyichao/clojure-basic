c6f3a9da39a7f3cba164725824c35e84ae568b7b
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-15 11:05:43 +0800

    flow control

---
+++ exploring-clojure/flow-control.clj
@@ -0,0 +1,53 @@
+(ns flow-control)
+
+(defn is-small? [number]
+  (if (< number 100) "yes"))
+
+(println (is-small? 50))
+(println (is-small? 150))
+
+(defn is-small-2? [number]
+  (if (< number 100) "yes" "no"))
+
+(println (is-small-2? 50))
+(println (is-small-2? 150))
+
+(defn is-small-3? [number]
+  (if (< number 100) 
+    "yes"
+    (do
+      (println "Saw a big number" number)
+      "no")))
+
+(println (is-small-3? 50))
+(println "----------------")
+(println (is-small-3? 150))
+
+; bindings: result is [], x is 10
+(println (loop [result [] x 10]
+  (if (zero? x)
+    result
+    (recur (conj result x) (dec x)))))
+
+(defn countdown [result x]
+  (if (zero? x)
+    result
+    (recur (conj result x) (dec x))))
+
+(println (countdown [] 6))
+
+(def x (into [] (take 5 (iterate dec 5))))
+
+(println "x:" x)
+
+(def y (into [] (drop-last (reverse (range 6)))))
+
+(println "y:" y)
+
+(def z (vec (reverse (rest (range 6)))))
+
+(println "z:" z)
+
+(println (range 6))
+(println (rest (range 6)))
+(println (reverse (rest (range 6))))
