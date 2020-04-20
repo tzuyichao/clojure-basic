321b846cea08a3717c532dc099089106c4b024f2
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-20 15:43:15 +0800

    question

---
+++ code-war/row-sum-odd-numbers.clj
@@ -0,0 +1,16 @@
+(ns row-sum-odd-numbers.core)
+
+(defn row-sum-odd-numbers [row-num]
+  # your code here
+)
+
+(ns row-sum-sample-tests
+  (require [clojure.test :refer :all]
+           [row-sum-odd-numbers.core :refer :all]))
+
+(deftest sample-tests
+  (is (= 1 (row-sum-odd-numbers 1)))
+  (is (= 125 (row-sum-odd-numbers 5)))
+  (is (= 343 (row-sum-odd-numbers 7))))
+
+(run-tests 'row-sum-sample-tests)
