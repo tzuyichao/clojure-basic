74ee9fd521f756b3176767af1ab459d357279b18
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-27 17:21:33 +0800

    chage ref

--- state/refs-examples.clj
+++ state/refs-examples.clj
@@ -1,3 +1,11 @@
 (def current-track (ref "Mars, the Bringer of War"))
 
 (println current-track)
+
+(println (deref current-track))
+
+(println @current-track)
+
+(dosync (ref-set current-track "Venus, the Bringer of Peace"))
+
+(println @current-track)
